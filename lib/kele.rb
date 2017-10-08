199bda243703ec5980d3044de432d4fb2e4ccee5
Brian Sung <bksung0708@gmail.com>
2017-10-07 23:21:32 -0700

    kele 4 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,7 +14,12 @@ class Kele
   end
 
   def get_me
-    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token } )
     @user_data = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token } )
+    @mentor_availability = JSON.parse(response.body)
   end
 end
