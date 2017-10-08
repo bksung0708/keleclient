2c2a49df68f9d4cfc0cefb084c3ee336fee85905
Brian Sung <bksung0708@gmail.com>
2017-10-07 22:25:22 -0700

    kele 3 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,20 @@
-require 'rubygems'
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    response = self.class.post("/sessions", body: { "email": email, "password": password } )
+    @email = email
+    @password = password
+    response = self.class.post("/sessions", body: { "email": @email, "password": @password } )
     @auth_token = response['auth_token']
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
   end
 end
